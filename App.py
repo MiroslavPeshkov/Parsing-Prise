b5502323f612d032440daa5157b088a2f93d30cf
Miroslav Peshkov <99148327+MiroslavPeshkov@users.noreply.github.com>
2022-09-10 17:24:56 +0300

    Update App.py

--- App.py
+++ App.py
@@ -70,7 +70,8 @@ def scrapping_avarage_price(s_all_final):
             links = soup.find_all('p', {'class', 'catalog-product__title'})
             links_ = [i.find('a') for i in links]
             links_all = ['https://www.tinko.ru' + i.get('href') for i in links_]
-            len_ = int(round((len(s_all_final) - 5) / 2, 0))
+            print(len(links_all))
+#             len_ = int(round((len(s_all_final) - 5) / 2, 0))
             st.write(len_)
             for l in links_all[:int(round((len(s_all_final) - 5) / 2, 0))]:
                 st.write('Now - ', l, 'for goods - ', goods)
